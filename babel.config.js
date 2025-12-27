2b4e228811771c65dee0d6c1fd9efb425e8de3c6
Elara Dev <dev@local>
2025-12-27 20:14:49 +0000

    fix: remove deprecated expo-router/babel plugin

--- babel.config.js
+++ babel.config.js
@@ -15,8 +15,7 @@ module.exports = function (api) {
           },
           extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
         }
-      ],
-      "expo-router/babel"
+      ]
     ]
   };
 };
